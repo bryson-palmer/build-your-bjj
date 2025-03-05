37bbca4c39138a46f4eeaa53551abdf7cdcc3b6a
Bryson Palmer <brysonpalmer@gmail.com>
2025-03-04 16:42:39 -0800

    Added mux data to video fields on create.

--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -1,21 +1,34 @@
 import { db } from "@/db"
 import { videos } from "@/db/schema"
+import { mux } from "@/lib/mux"
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init"
 
 export const videosRouter = createTRPCRouter({
   create: protectedProcedure.mutation(async ({ ctx }) => {
     const { id: userId } = ctx.user
 
+    const upload = await mux.video.uploads.create({
+      new_asset_settings: {
+        passthrough: userId,
+        playback_policy: ["public"],
+      },
+      cors_origin: "*" // TODO: in production, set to the url
+    })
+
+    // Video creation
     const [video] = await db
       .insert(videos)
       .values({
         userId,
-        title: "Untitled"
+        title: "Untitled",
+        muxStatus: "waiting",
+        muxUploadId: upload.id,
       })
       .returning()
 
       return {
-        video: video
+        video: video,
+        url: upload.url,
       }
   })
 })
