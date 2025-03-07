fe42c63d80b690b81ac0d0cbdd560eb3694eadd3
Bryson Palmer <brysonpalmer@gmail.com>
2025-03-06 16:44:56 -0800

    Finished all video webhook event routes for POST request.

--- src/app/api/videos/webhook/route.ts
+++ src/app/api/videos/webhook/route.ts
@@ -2,6 +2,7 @@ import { eq } from "drizzle-orm"
 import { headers } from "next/headers"
 import {
   VideoAssetCreatedWebhookEvent,
+  VideoAssetDeletedWebhookEvent,
   VideoAssetErroredWebhookEvent,
   VideoAssetReadyWebhookEvent,
   VideoAssetTrackReadyWebhookEvent,
@@ -15,6 +16,7 @@ const SIGNING_SECRET = process.env.MUX_WEBHOOK_SECRET!
 
 type WebhookEvent =
 | VideoAssetCreatedWebhookEvent
+| VideoAssetDeletedWebhookEvent
 | VideoAssetErroredWebhookEvent
 | VideoAssetReadyWebhookEvent
 | VideoAssetTrackReadyWebhookEvent
@@ -62,6 +64,93 @@ export const POST = async (request: Request) => {
           muxStatus: data.status,
         })
         .where(eq(videos.muxUploadId, data.upload_id))
+      break
+    }
+
+    case "video.asset.ready": {
+      const data = payload.data as VideoAssetReadyWebhookEvent["data"]
+      const playbackId = data.playback_ids?.[0].id
+
+      // Mux error: upload or playback id doesn't exist in Mux
+      if (!data.upload_id) {
+        return new Response("Missing upload ID", { status: 400 })
+      }
+      if (!playbackId) {
+        return new Response("Missing playback ID", { status: 400 })
+      }
+
+      const thumbnailUrl = `https://image.mux.com/${playbackId}/thumbnail.jpg`
+      const previewUrl = `https://image.mux.com/${playbackId}/animated.gif`
+
+      const duration = data.duration ? Math.round(data.duration * 1000) : 0
+
+      await db
+        .update(videos)
+        .set({
+          muxAssetId: data.id,
+          muxPlaybackId: playbackId,
+          muxStatus: data.status,
+          thumbnailUrl,
+          previewUrl,
+          duration,
+        })
+        .where(eq(videos.muxUploadId, data.upload_id))
+      break
+    }
+
+    case "video.asset.errored": {
+      const data = payload.data as VideoAssetErroredWebhookEvent["data"]
+
+      // Mux error: upload id doesn't exist in Mux
+      if (!data.upload_id) {
+        return new Response("Missing upload ID", { status: 400 })
+      }
+
+      await db
+        .update(videos)
+        .set({
+          muxStatus: data.status
+        })
+        .where(eq(videos.muxUploadId, data.upload_id))
+      break
+    }
+
+    case "video.asset.deleted": {
+      const data = payload.data as VideoAssetDeletedWebhookEvent["data"]
+
+      // Mux error: upload id doesn't exist in Mux
+      if (!data.upload_id) {
+        return new Response("Missing upload ID", { status: 400 })
+      }
+
+      await db
+        .delete(videos)
+        .where(eq(videos.muxUploadId, data.upload_id))
+      break
+    }
+
+    case "video.asset.track.ready": {
+      const data = payload.data as VideoAssetTrackReadyWebhookEvent["data"] & {
+        asset_id: string,
+      }
+
+      // Typescript incorrectly says that asset_id does not exist
+      const assetId = data.asset_id
+      const trackId = data.id
+      const status = data.status
+
+      // Mux error: asset id doesn't exist in Mux
+      if (!assetId) {
+        return new Response("Missing asset ID", { status: 400 })
+      }
+
+      await db
+        .update(videos)
+        .set({
+          muxTrackId: trackId,
+          muxTrackStatus: status
+        })
+        .where(eq(videos.muxAssetId, assetId))
       break
     }
   }
