13ab05e9089f082cda0620f680e8e08c102de0fa
Bryson Palmer <brysonpalmer@gmail.com>
2025-04-02 16:57:23 -0700

    Updated VideoOwner to use subscription hook.

--- src/modules/videos/ui/components/video-owner.tsx
+++ src/modules/videos/ui/components/video-owner.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button"
 import { UserAvatar } from "@/components/user-avatar"
 
 import { UserInfo } from "@/modules/users/ui/components/user-info"
+import { useSubscription } from "@/modules/subscriptions/hooks/use-subscription"
 import { SubscriptionButton } from "@/modules/subscriptions/ui/components/subscription-button"
 
 import { VideoGetOneOutput } from "../../types"
@@ -18,7 +19,12 @@ export const VideoOwner = ({
   user,
   videoId,
 }: VideoOwnerProps) => {
-  const { userId: clerkUserId } = useAuth()
+  const { userId: clerkUserId, isLoaded } = useAuth()
+  const { isPending, onClick } = useSubscription({
+    userId: user.id,
+    isSubscribed: user.viewerSubscribed,
+    fromVideoId: videoId,
+  })
 
   return (
     <div className="flex items-center sm:items-start justify-between sm:justify-start gap-3 min-w-0">
@@ -28,8 +34,7 @@ export const VideoOwner = ({
           <div className="flex flex-col gap-1 min-w-0">
             <UserInfo size="lg" name={user.name} />
             <span className="text-sm text-muted-foreground line-clamp-1">
-              {/* TODO: properly fill subsriber count */}
-              {0} subscribers 
+              {user.subscriberCount} subscribers 
             </span>
           </div>
         </div>
@@ -46,9 +51,9 @@ export const VideoOwner = ({
         </Button>
       ) : (
         <SubscriptionButton
-          onClick={() => {}}
-          disabled={false}
-          isSubscribed={false}
+          onClick={onClick}
+          disabled={isPending || !isLoaded}
+          isSubscribed={user.viewerSubscribed}
           className="flex-none"
         />
       )}
