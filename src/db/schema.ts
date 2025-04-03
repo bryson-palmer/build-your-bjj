6771f9e5a96233d2c1004be2a43b9e0653e12c3b
Bryson Palmer <brysonpalmer@gmail.com>
2025-04-03 14:27:02 -0700

    Misc. subscription relation preference name change.

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -54,12 +54,12 @@ export const subscriptions = pgTable("subscriptions", {
 // Application level relationship between the user and videos
 // OPTIONAL: if using relational queries or not using postgres database
 export const subscriptionRelations = relations(subscriptions, ({ one }) => ({
-  viewerId: one(users, {
+  viewer: one(users, {
     fields: [subscriptions.viewerId],
     references: [users.id],
     relationName: "subscriptions_viewer_id_fkey",
   }), 
-  creatorId: one(users, {
+  creator: one(users, {
     fields: [subscriptions.creatorId],
     references: [users.id],
     relationName: "subscriptions_creator_id_fkey",
