2d7999e6c6027b40a9a43cd36c9f84145c161265
Bryson Palmer <brysonpalmer@gmail.com>
2025-04-22 11:42:22 -0700

    Update: extend comments schema and commentRelations to include parentId foreign key.

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,6 @@
 import { relations } from "drizzle-orm"
 import {
+  foreignKey,
   integer,
   pgEnum,
   pgTable,
@@ -137,12 +138,20 @@ export const videoRelations = relations(videos, ({ one, many }) => ({
 
 export const comments = pgTable("comments", {
   id: uuid("id").primaryKey().defaultRandom(),
+  parentId: uuid("parent_id"),
   userId: uuid("user_id").references(() => users.id, { onDelete: "cascade" }).notNull(),
   videoId: uuid("video_id").references(() => videos.id, { onDelete: "cascade" }).notNull(),
   value: text("value").notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
-})
+}, (t) => [
+  foreignKey({
+    columns: [t.parentId],
+    foreignColumns: [t.id],
+    name: "comments_parent_id_fkey",
+  })
+  .onDelete("cascade")
+])
 
 export const commentInsertSchema = createInsertSchema(comments)
 export const commentFormSchema = commentInsertSchema.omit({ userId: true })
@@ -158,7 +167,15 @@ export const commentRelations = relations(comments, ({ one, many }) => ({
     fields: [comments.videoId],
     references: [videos.id]
   }),
+  parent: one(comments, {
+    fields: [comments.parentId],
+    references: [comments.id],
+    relationName: "comments_parent_id_fkey"
+  }),
   reactions: many(commentReactions),
+  replies: many(comments, {
+    relationName: "comments_parent_id_fkey",
+  })
 }))
 
 export const commentReactions = pgTable("comment_reactions", {
