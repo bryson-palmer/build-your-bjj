0ef91ff120634e8018db4be3240d969ca32941fd
Bryson Palmer <brysonpalmer@gmail.com>
2025-04-29 17:34:08 -0700

    Update: video id page with suggestions get many prefetch.

--- src/app/(home)/videos/[videoId]/page.tsx
+++ src/app/(home)/videos/[videoId]/page.tsx
@@ -17,6 +17,7 @@ const Page = async ({
 
   void trpc.videos.getOne.prefetch({ id: videoId })
   void trpc.comments.getMany.prefetchInfinite({ videoId, limit: DEFAULT_LIMIT })
+  void trpc.suggestions.getMany.prefetchInfinite({ videoId, limit: DEFAULT_LIMIT })
 
   return (
     <HydrateClient>
