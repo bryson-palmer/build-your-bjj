a502c5f01a256a5117910239d6287a616207022f
Bryson Palmer <brysonpalmer@gmail.com>
2025-05-02 15:27:05 -0700

    Update: suggestions section with video skeleton ui.

--- src/modules/videos/ui/sections/suggestions-section.tsx
+++ src/modules/videos/ui/sections/suggestions-section.tsx
@@ -4,12 +4,29 @@ import { trpc } from "@/trpc/client"
 import { DEFAULT_LIMIT } from "@/constants"
 import { InfiniteScroll } from "@/components/infinite-scroll"
 
-import { VideoRowCard } from "../components/video-row-card"
-import { VideoGridCard } from "../components/video-grid-card"
+import { VideoGridCard, VideoGridCardSkeleton } from "../components/video-grid-card"
+import { VideoRowCard, VideoRowCardSkeleton } from "../components/video-row-card"
 
 interface SuggestionsSectionProps {
   videoId: string,
   isManual?: boolean,
+}
+
+const SuggestionsSectionSkeleton = () => {
+  return (
+    <>
+      <div className="hidden md:block space-y-3">
+        {Array.from({ length: 6 }).map((_, index) => (
+          <VideoRowCardSkeleton key={index} size={"compact"} />
+        ))}
+      </div>
+      <div className="block md:hidden space-y-10">
+        {Array.from({ length: 6 }).map((_, index) => (
+          <VideoGridCardSkeleton key={index} />
+        ))}
+      </div>
+    </>
+  )
 }
 
 export const SuggestionsSection = ({
