ef51431f3eb3800c647ffc64f3b496d54e5ebc06
Bryson Palmer <brysonpalmer@gmail.com>
2025-05-02 15:32:56 -0700

    Update: studio form w/revalidate logic/ui. For mux webhook edgecase status:stuck.

--- src/modules/studio/ui/sections/form-section.tsx
+++ src/modules/studio/ui/sections/form-section.tsx
@@ -153,6 +153,17 @@ const FormSectionSuspense = ({
     }
   })
 
+  const revalidate = trpc.videos.revalidate.useMutation({
+    onSuccess: () => {
+      utils.studio.getMany.invalidate()
+      utils.studio.getOne.invalidate({ id: videoId })
+      toast.success("Video revalidated")
+    },
+    onError: () => {
+      toast.error("Something went wrong")
+    }
+  })
+
   const restoreThumbnail = trpc.videos.restoreThumbnail.useMutation({
     onSuccess: () => {
       utils.studio.getMany.invalidate()
@@ -229,6 +240,10 @@ const FormSectionSuspense = ({
                   </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align="end">
+                  <DropdownMenuItem onClick={() => revalidate.mutate({ id: videoId })}>
+                    <RotateCcwIcon className="size-4 mr-2" />
+                    Revalidate
+                  </DropdownMenuItem>
                   <DropdownMenuItem onClick={() => remove.mutate({ id: videoId })}>
                     <TrashIcon className="size-4 mr-2" />
                     Delete
