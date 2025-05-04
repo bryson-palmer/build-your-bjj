adf78be41c0a34ac7aaf65585f66a5780f0308e8
Bryson Palmer <brysonpalmer@gmail.com>
2025-05-04 11:07:54 -0700

    Update: search input with persistant search params and setting url.

--- src/modules/home/ui/components/home-navbar/search-input.tsx
+++ src/modules/home/ui/components/home-navbar/search-input.tsx
@@ -1,14 +1,19 @@
 "use client"
 
 import { useState } from "react"
-import { useRouter } from "next/navigation"
 import { SearchIcon, XIcon } from "lucide-react"
+import { useRouter, useSearchParams } from "next/navigation"
 
 import { APP_URL } from "@/constants"
 import { Button } from "@/components/ui/button"
 
 export const SearchInput = () => {
   const router = useRouter()
+  const searchParams = useSearchParams()
+
+  const query = searchParams.get("query") || ""
+  const categoryId = searchParams.get("categoryId") || ""
+
   const [value, setValue] = useState(query)
 
   // TODO: need to handle when clearing query and not updating the url
@@ -20,6 +25,10 @@ export const SearchInput = () => {
     const newQuery = value.trim()
 
     url.searchParams.set("query", encodeURIComponent(newQuery))
+
+    if (categoryId) {
+      url.searchParams.set("categoryId", categoryId)
+    }
 
     if (newQuery === "") {
       url.searchParams.delete("query")
