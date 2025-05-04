06d95b93188f46ea8e441e0c87a4f670ae47d611
Bryson Palmer <brysonpalmer@gmail.com>
2025-05-04 11:08:34 -0700

    Update: results section to be a suspense component with loading skeleton.

--- src/modules/search/ui/sections/results-section.tsx
+++ src/modules/search/ui/sections/results-section.tsx
@@ -1,19 +1,53 @@
 "use client"
 
+import { Suspense } from "react"
 import { trpc } from "@/trpc/client"
 import { DEFAULT_LIMIT } from "@/constants"
 import { useIsMobile } from "@/hooks/use-mobile"
+import { ErrorBoundary } from "react-error-boundary"
 
 import { InfiniteScroll } from "@/components/infinite-scroll"
-import { VideoRowCard } from "@/modules/videos/ui/components/video-row-card"
-import { VideoGridCard } from "@/modules/videos/ui/components/video-grid-card"
+import { VideoRowCard, VideoRowCardSkeleton } from "@/modules/videos/ui/components/video-row-card"
+import { VideoGridCard, VideoGridCardSkeleton } from "@/modules/videos/ui/components/video-grid-card"
 
 interface ResultsSectionProps {
   query: string | undefined,
   categoryId: string | undefined,
 }
 
-export const ResultsSection = ({
+export const ResultsSection = (props: ResultsSectionProps) => {
+  return (
+    <Suspense
+      key={`${props.query}-${props.categoryId}`}
+      fallback={<ResultsSectionSkeleton
+    />}>
+      <ErrorBoundary fallback={<p>Error</p>}>
+        <ResultsSectionSuspense {...props} />
+      </ErrorBoundary>
+    </Suspense>
+  )
+}
+
+const ResultsSectionSkeleton = () => {
+  return (
+    <div>
+      {/* Desktop */}
+      <div className="hidden flex-col gap-4 md:flex">
+        {Array.from({ length: 5}).map((_, index) => (
+          <VideoRowCardSkeleton key={index} />
+        ))}
+      </div>
+      {/* Mobile */}
+      <div className="flex flex-col gap-4 p-4 gap-y-10 pt-6 md:hidden">
+      {Array.from({ length: 5}).map((_, index) => (
+          <VideoGridCardSkeleton key={index} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+const ResultsSectionSuspense = ({
   query,
   categoryId,
 }: ResultsSectionProps) => {
