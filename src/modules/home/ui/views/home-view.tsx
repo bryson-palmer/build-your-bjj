cf555a41b89b054ce8666770ed88c923b9da6cd4
Bryson Palmer <brysonpalmer@gmail.com>
2025-05-09 14:01:28 -0700

    Update: home page with new VideosSection component.

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,4 +1,5 @@
 import { CategoriesSection } from "../sections/categories-section"
+import { VideosSection } from "../sections/videos-section"
 
 // If categoryId becomes an [] of string ids, would need to update here
 interface HomeViewProps {
@@ -10,6 +11,7 @@ export const HomeView = ({ categoryId }: HomeViewProps) => {
   return (
     <div className="max-w-[2400px] mx-auto mb-10 px-4 pt-2.5 flex flex-col gap-y-6">
       <CategoriesSection categoryId={categoryId} />
+      <VideosSection categoryId={categoryId} />
     </div>
   )
 }
