7ddcef62b3039074b29932ef2d6e255fca426da3
Bryson Palmer <brysonpalmer@gmail.com>
2025-06-11 16:22:50 -0700

    New: subscriptions section for home-sidebar to render short list of subs and links to sub page or users page.

---
+++ src/modules/home/ui/components/home-sidebar/subscriptions-section.tsx
@@ -0,0 +1,86 @@
+"use client"
+
+import Link from "next/link"
+import { trpc } from "@/trpc/client"
+import { ListIcon } from "lucide-react"
+import { usePathname } from "next/navigation"
+
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem
+} from "@/components/ui/sidebar"
+import { DEFAULT_LIMIT } from "@/constants"
+import { Skeleton } from "@/components/ui/skeleton"
+import { UserAvatar } from "@/components/user-avatar"
+
+export const LoadingSkeleton = () => (
+  <>
+    {[1, 2, 3, 4].map(i => (
+      <SidebarMenuItem key={i}>
+        <SidebarMenuButton disabled>
+          <Skeleton className="size-6 rounded-full shrink-0" />
+          <Skeleton className="h-4 w-full" />
+        </SidebarMenuButton>
+      </SidebarMenuItem>
+    ))}
+  </>
+)
+
+export const SubscriptionsSection = () => {
+  const pathname = usePathname()
+  const { data, isLoading } = trpc.subscriptions.getMany.useInfiniteQuery(
+    { limit: DEFAULT_LIMIT },
+    { getNextPageParam: (lastPage) => lastPage.nextCursor }
+  )
+
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel>Subscriptions</SidebarGroupLabel>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {isLoading && <LoadingSkeleton />}
+          {!isLoading && data?.pages
+            .flatMap((page) => page.items)
+            .map(subscription => (
+            <SidebarMenuItem key={`${subscription.creatorId}-${subscription.viewerId}`}>
+              <SidebarMenuButton
+                asChild
+                tooltip={subscription.user.name}
+                isActive={pathname === `/users/${subscription.user.id}`}
+              >
+                <Link href={`/users/${subscription.user.id}`} className="flex items-center gap-4">
+                  <UserAvatar
+                    imageUrl={subscription.user.imageUrl}
+                    name={subscription.user.name}
+                    size="xs"
+                  />
+                  <span className="text-sm">{subscription.user.name}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+          {!isLoading && (
+            <SidebarMenuItem>
+              <SidebarMenuButton
+                asChild
+                isActive={pathname === "/subscriptions"}
+              >
+                <Link
+                  href="/subscriptions"
+                  className="flex items-center gap-4"
+                >
+                  <ListIcon className="size-4" />
+                  <span className="text-sm">All subscriptions</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          )}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  )
+}
