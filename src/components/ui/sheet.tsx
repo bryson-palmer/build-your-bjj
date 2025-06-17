da431d9b32c6dbad63ddf4e28eb50ad234451fec
Bryson Palmer <brysonpalmer@gmail.com>
2025-06-16 19:34:38 -0700

    Bug: fixing shadcn/ui's sheet content accessibility issue locally.

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -56,18 +56,20 @@ interface SheetContentProps
 const SheetContent = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Content>,
   SheetContentProps
->(({ side = "right", className, children, ...props }, ref) => (
+>(({ side = "right", className, children, title, ...props }, ref) => (
   <SheetPortal>
     <SheetOverlay />
     <SheetPrimitive.Content
       ref={ref}
       className={cn(sheetVariants({ side }), className)}
+      aria-describedby={title}
       {...props}
     >
       <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
         <X className="h-4 w-4" />
         <span className="sr-only">Close</span>
       </SheetPrimitive.Close>
+      <SheetPrimitive.Title className="sr-only">{title}</SheetPrimitive.Title>
       {children}
     </SheetPrimitive.Content>
   </SheetPortal>
